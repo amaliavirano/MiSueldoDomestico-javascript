72ff457b2ca5d7520cc90bcae241fe9ce10685c5
Amalia Virano <amaliavirano@gmail.com>
2023-09-16 09:50:39 -0300

    modificaciones

--- js/main.js
+++ js/main.js
@@ -1,35 +1,53 @@
 
 let CategoriaSueldo = Number(prompt("Por favor, ingresar categoría profesional"));
-let horasTrabajadas = Number(prompt("Por favor, ingresar horas trabajadas"));
+
+let horasTrabajadas = Number(prompt("Por favor, ingresar horas trabajadas en el mes"));
+
 let sueldoBase = 1;
 
+const empleadasDomesticas = [
+    { categoria: "Primera Categoria", Descripcion: "tareas de supervisión", Sueldo: 159074 },
+    { categoria: "Segunda Categoria", Descripcion: "tareas específicas", Sueldo: 147788.50 },
+    { categoria: "Tercera Categoria", Descripcion: "tareas como casera", Sueldo: 144191.5},
+    { categoria: "Cuarta Categoria", Descripcion: "tareas de cuidado de personas", Sueldo: 144191.5},
+    { categoria: "Quinta Categoria", Descripcion: "tareas generales", Sueldo: 129670 },
+];
+
+
 if (CategoriaSueldo == 1) {
-    sueldoBase = (159074 / 192 * horasTrabajadas)
+    sueldoBase = (empleadasDomesticas[CategoriaSueldo - 1].Sueldo / 192 * horasTrabajadas)
 
-    console.log("El sueldo total de la empleada doméstica es " + sueldoBase)
+    alert("Tu sueldo total es $" + sueldoBase + " correspondiente a la " + empleadasDomesticas[CategoriaSueldo - 1].categoria + " que realiza  " + empleadasDomesticas[CategoriaSueldo - 1].Descripcion)
+    console.log("El sueldo calculado es " + sueldoBase);
+
 } else if (CategoriaSueldo == 2) {
-    sueldoBase = (147788.50 / 192 * horasTrabajadas)
+    sueldoBase = (empleadasDomesticas[CategoriaSueldo - 1].Sueldo / 192 * horasTrabajadas)
 
-    console.log("El sueldo total de la empleada doméstica es " + sueldoBase)
-    alert("El sueldo calculado es " + sueldoBase)
-} else if (CategoriaSueldo == 3 || CategoriaSueldo == 4) {
-    sueldoBase = (144191.50 / 192 * horasTrabajadas)
+    alert("Tu sueldo total es $" + sueldoBase + " correspondiente a la " + empleadasDomesticas[CategoriaSueldo - 1].categoria + " que realiza  " + empleadasDomesticas[CategoriaSueldo - 1].Descripcion)
+    console.log("El sueldo calculado es " + sueldoBase);
 
-    console.log("El sueldo total de la empleada doméstica es " + sueldoBase)
-    alert("El sueldo calculado es " + sueldoBase)
+} else if (CategoriaSueldo == 3) {
+    sueldoBase = (empleadasDomesticas[CategoriaSueldo - 1].Sueldo / 192 * horasTrabajadas)
+
+    alert("Tu sueldo total es $" + sueldoBase + " correspondiente a la " + empleadasDomesticas[CategoriaSueldo - 1].categoria + " que realiza  " + empleadasDomesticas[CategoriaSueldo - 1].Descripcion)
+    console.log("El sueldo calculado es " + sueldoBase);
+
+} else if (CategoriaSueldo == 4) {
+    sueldoBase = (empleadasDomesticas[CategoriaSueldo - 1].Sueldo / 192 * horasTrabajadas)
+
+    alert("Tu sueldo total es $" + sueldoBase + " correspondiente a la " + empleadasDomesticas[CategoriaSueldo - 1].categoria + " que realiza  " + empleadasDomesticas[CategoriaSueldo - 1].Descripcion)
+    console.log("El sueldo calculado es " + sueldoBase);
+
 } else if (CategoriaSueldo == 5) {
-    sueldoBase = (129670 / 192 * horasTrabajadas)
+    sueldoBase = (empleadasDomesticas[CategoriaSueldo - 1].Sueldo / 192 * horasTrabajadas)
 
-    console.log("El sueldo total de la empleada doméstica es " + sueldoBase)
-    alert("El sueldo calculado es " + sueldoBase)
+    alert("Tu sueldo total es $" + sueldoBase + " correspondiente a la " + empleadasDomesticas[CategoriaSueldo - 1].categoria + " que realiza  " + empleadasDomesticas[CategoriaSueldo - 1].Descripcion)
+    console.log("El sueldo calculado es " + sueldoBase);
 
 } else {
     alert("Lo siento, no existe la categoría profesional " + CategoriaSueldo);
 
-
 }
-
-
 
 function SueldoTotal(_SueldoCalculado, _Antiguedad) {
     _SueldoCalculado = parseFloat(_SueldoCalculado);
@@ -40,18 +58,28 @@ function SueldoTotal(_SueldoCalculado, _Antiguedad) {
         return;
     }
 
-    var resultado = _SueldoCalculado * (1 + _Antiguedad / 100);
+    let resultado = _SueldoCalculado * (1 + _Antiguedad / 100);
 
     alert(" El sueldo calculado con antiguedad es igual a " + resultado);
     console.log("El sueldo total de la empleada doméstica es " + resultado);
 }
 
 
-var numero1 = prompt("Ingresa sueldo calculado");
-var numero2 = prompt("Ingresa su antiguedad en años");
+let numero1 = prompt("Ingresa sueldo calculado");
+let numero2 = prompt("Ingresa su antiguedad en años");
 
 
 SueldoTotal(numero1, numero2);
+
+
+
+
+
+
+
+
+
+
 
 
 
